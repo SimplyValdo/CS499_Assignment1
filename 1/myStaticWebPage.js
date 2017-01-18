292af908554bc1cafe584601bc5e36e88a7da470
SimplyValdo <simplyvaldo@gmail.com>
2017-01-17 18:54:51 -0800

    Add files via upload

---
+++ 1/myStaticWebPage.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Valdo on 1/17/2017.
+ */
+
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendfile('./homePage.html')
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
